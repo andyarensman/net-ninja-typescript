64687e00fcea653daaa60d8ecb2a286c7dfc40a8
andyarensman <86622025+andyarensman@users.noreply.github.com>
2023-02-08 12:42:11 -0800

    lesson 8 - function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('test');
+let greet = () => {
+    console.log('hello, world');
+};
+let mssg;
+const add = (a, b, c) => {
+    console.log(a + b);
+};
+const divide = (a, b, c = 10) => {
+    console.log(a / b);
+};
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,20 @@
-console.log('test')
+let greet = () => {
+  console.log('hello, world');
+}
+
+let mssg: Function;
+
+const add = (a: number, b: number, c?: number | string) => {
+  console.log(a + b)
+}
+
+
+const divide = (a: number, b: number, c: number | string = 10) => {
+  console.log(a / b)
+}
+
+const minus = (a: number, b: number): number => {
+  return a + b
+}
+
+let result = minus(10, 7);

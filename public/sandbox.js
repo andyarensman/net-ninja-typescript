d5099635115bd3d2b49950d317b6be60699079fd
andyarensman <86622025+andyarensman@users.noreply.github.com>
2023-02-09 15:31:02 -0800

    lesson 11 - DOM and Type Casting

--- public/sandbox.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-//let greet: Function;
-let greet;
-let calc;
-let logDetails;
--- src/sandbox.ts
+++
@@ -1,9 +0,0 @@
-//let greet: Function;
-
-let greet: (a: string, b: string) => void;
-
-let calc: (a: number, b: number, c: string) => number;
-
-let logDetails: (obj: {name: string, age: number}) => void;
-
-type person = {name: string, age: number};
